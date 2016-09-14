40280604fa2b4b6d03d322bebf62fb3313583b1e
buckwheat <comp322012016@mail.ru>
2016-09-14 17:42:24 +0300

    px

---
+++ main.js
@@ -0,0 +1,32 @@
+var z,x;
+z=0;
+if (z>0) {
+x=1;
+}
+else if (z<0) {
+	x=4;
+}
+else {
+	x=8;
+}
+console.log(x);
+  
+ var b,c;
+b=0;
+switch (b>0) {
+	case true:c=1;   
+	break;
+	case false:c=2;
+	break;
+	default:c=3;
+	break;
+}
+console.log(c);
+
+var x,y;
+x=7;
+y=3;
+for(var i=0;i<78;i++) {
+x=x+y;
+}
+console.log(x);
