e2eb2b4f879c04b015ded9fb7c037d334d26ac6a
buckwheat <comp322012016@mail.ru>
2016-09-15 17:42:43 +0300

    dsfds

--- main.js
+++ main.js
@@ -30,3 +30,36 @@ for(var i=0;i<78;i++) {
 x=x+y;
 }
 console.log(x);
+
+
+var stonik={
+	"atlantic":777,
+	"num":77.77,
+	"str_one":'Pasha Tehnik',
+	"logic":true,
+}
+for (var a in stonik)
+	console.log(a);
+
+delete stonik["num"];
+console.log(stonik);
+
+stonik.abcd=7777;
+console.log(stonik);
+
+var stonik={
+	"azazel":111,
+	"velial":"squad",
+	"mefisto":3.20,
+	"lucifer":true,
+	"LaVey":{bible:111,church:"burn"}
+}
+console.log(stonik.LaVey.bible);
+
+
+var exampleobject=Object.create({x:5,y:"text",z:3.8,s:false,f:{one:1,two:2}})
+for (var l in exampleobject)
+	console.log(l);
+
+var obj = new Object({q:8,w:"asd",e:4.4,r:true,t:{aaa:111,two:222}})
+console.log(obj);
